711be18dd925e0028aa3096077db3c9c7ca9a156
Eduardo Sobrino Cortes <esobrino@car15s4.42barcelona.com>
2026-04-24 21:14:45 +0200

    ex0 completed

---
+++ ex0/ft_garden_intro.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+def main() -> None:
+    print("=== Welcome to My Garden ===")
+    input('Plant: ')
+    input('Height: ')
+    input('Age: ')
+    print("\n=== End of Program ===")
+
+
+if __name__ == "__main__":
+    main()
